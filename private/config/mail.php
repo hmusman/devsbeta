eba0cdd55bc3622e7ee0ebfb7677c259457dfedb
hmusman <hmusman.it@gmail.com>
2020-01-06 14:22:25 +0500

    info@example.com spaming
    its spaming emails

--- private/config/mail.php
+++ private/config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@devsbeta.com'),
         'name' => env('MAIL_FROM_NAME', 'DevsBeta'),
     ],
 
